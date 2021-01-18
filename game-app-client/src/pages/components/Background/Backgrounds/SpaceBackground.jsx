2a6a10509b5cd5b4370601bcdeeffec87b4cbca1
sec05 <58738123+sec05@users.noreply.github.com>
2021-01-18 11:10:07 -0500

    adding spaceship sprite to background

--- game-app-client/src/pages/components/Background/Backgrounds/SpaceBackground.jsx
+++ game-app-client/src/pages/components/Background/Backgrounds/SpaceBackground.jsx
@@ -1,39 +1,51 @@
-import React, {useEffect, useRef, useState} from 'react'
-import {GameCanvas} from "../../../../helpers/SimpleCanvasLibrary.js";
+import React, { useEffect, useRef, useState } from "react";
+import { GameCanvas, Sprite } from "../../../../helpers/SimpleCanvasLibrary.js";
+import { spaceshipSprite } from "../../../../assets/spaceshipSprite.js";
 export default function SpaceBackground() {
-    
-    const canvasRef = useRef(null);
-    const [canvasWidth, updateCanvasWidth] = useState(window.innerWidth);
-    const [canvasHeight, updateCanvasHeight] = useState(window.innerHeight); 
-    
+  const canvasRef = useRef(null);
+  const [canvasWidth, updateCanvasWidth] = useState(window.innerWidth);
+  const [canvasHeight, updateCanvasHeight] = useState(window.innerHeight);
 
-   const updateCanvasSize = () =>
-   {
-        updateCanvasWidth(window.innerWidth);
-        updateCanvasHeight(window.innerHeight);
-   }
-    
-    useEffect(() => {
-        const canvas = new GameCanvas("bkg")
-        const drawOnCanvas = () =>
-        {
-           /* canvas.addDrawing(
-                function ({ctx, elapsed, width, height}) {
-                    let x = (100 * elapsed/1000) % width;
-                    let y = height/2
-                    ctx.beginPath();
-                    ctx.arc(x,y,20,0,Math.PI*2);
-                    ctx.stroke();
-                    console.log(width, height)
-                });
-                canvas.run();*/
-        }
-        drawOnCanvas();
-        window.addEventListener("resize",updateCanvasSize,false);
-    }, [])
-    return (
-        <div>
-            <canvas id="bkg" ref={canvasRef} width={canvasWidth} height={canvasHeight}/>
-        </div>
-    )
+  const updateCanvasSize = () => {
+    updateCanvasWidth(window.innerWidth);
+    updateCanvasHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    const canvas = new GameCanvas("bkg");
+    const drawOnCanvas = () => {
+      const spaceShip = new Sprite({
+        src: spaceshipSprite,
+        frameWidth: 310,
+        frameHeight: 330,
+        x: 100,
+        y: 100,
+        targetHeight: 82.5,
+        targetWidth: 77.5,
+        frame: 0,
+        frameRate: 0,
+        angle: 2*Math.PI/9,
+    update({sprite, stepTime}){
+      sprite.angle +=1*stepTime/500 
+     
+    },
+});
+
+      canvas.addDrawing(spaceShip);
+
+      canvas.run();
+    };
+    drawOnCanvas();
+    window.addEventListener("resize", updateCanvasSize, false);
+  }, []);
+  return (
+    <div>
+      <canvas
+        id="bkg"
+        ref={canvasRef}
+        width={canvasWidth}
+        height={canvasHeight}
+      />
+    </div>
+  );
 }
